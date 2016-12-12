4119255f3af35948bb33a30959c7fae814e4a9ec
Max Britvin <maksbritvin@gmail.com>
2016-12-12 21:46:10 +0300

    Table view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import (QMainWindow, QPushButton, QApplication, QWidget, QGridLayout, QAction,
-                             qApp, QFileDialog, QComboBox,QLineEdit, QLabel)
+                             qApp, QFileDialog, QComboBox, QLineEdit, QLabel, QTableWidget, QTableWidgetItem)
 
 
 class MainWindow(QMainWindow):
@@ -12,30 +12,56 @@ class MainWindow(QMainWindow):
                             'NOTCONSOL']
         self.consolsettings = ['[Active]', '[PCON]', '[POWN]', '[Method]', '[Consol1]']
         self.month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        self.scenario = ["ACT", "BDG", "TAX"]
         self.years = [year for year in range(2012, 2031)]
 
         # Окно
-        self.setGeometry(300,300,300,300)
+        self.setGeometry(500, 300, 1100, 300)
         self.setWindowTitle("Consolidation form helper")
         self.mainwidget = QWidget()
         self.setCentralWidget(self.mainwidget)
         self.layout_grid = QGridLayout()
         self.mainwidget.setLayout(self.layout_grid)
 
-        # Элементы в окне
-        self.Entity_label = QLabel("Entity")
-        self.Entity = QLineEdit()
-        self.ICP_label = QLabel("ICP")
-        self.ICP = QLineEdit()
-        self.year_from_label = QLabel('Start year')
-        self.year_from = QComboBox()
-        self.year_to_label = QLabel('End year')
-        self.year_to = QComboBox()
-        # Расположение
-        self.layout_grid.addWidget(self.Entity_label, 0, 0)
-        self.layout_grid.addWidget(self.ICP_label, 0, 1)
-        self.layout_grid.addWidget(self.Entity, 1, 0)
-        self.layout_grid.addWidget(self.ICP, 1, 1)
+        # Комбобоксы
+        self.consolmethod_box = QComboBox()
+        self.consolmethod_box.addItems(self.consolmetod)
+        self.scenario_box = QComboBox()
+        self.scenario_box.addItems(self.scenario)
+        # таблица
+        self.table = QTableWidget()
+        self.table.setRowCount(2)
+        self.table.setColumnCount(10)
+        self.table.setColumnWidth(8, 120)
+        self.table.setColumnHidden(9, True)
+
+        # Имена колонок
+        self.table.setHorizontalHeaderItem(0, QTableWidgetItem("Scenario"))
+        self.table.setHorizontalHeaderItem(1, QTableWidgetItem("Month/year start"))
+        self.table.setHorizontalHeaderItem(2, QTableWidgetItem("Month/year end"))
+        self.table.setHorizontalHeaderItem(3, QTableWidgetItem("Entity"))
+        self.table.setHorizontalHeaderItem(4, QTableWidgetItem("ICP"))
+        self.table.setHorizontalHeaderItem(5, QTableWidgetItem("[Active]"))
+        self.table.setHorizontalHeaderItem(6, QTableWidgetItem('[PCON]'))
+        self.table.setHorizontalHeaderItem(7, QTableWidgetItem('[POWN]'))
+        self.table.setHorizontalHeaderItem(8, QTableWidgetItem('[Method]'))
+        self.table.setHorizontalHeaderItem(9, QTableWidgetItem('[Consol1]'))
+
+        # Заполнение первой строки таблицы
+        self.table.setCellWidget(0, 8, self.consolmethod_box)
+        self.table.setCellWidget(0, 0, self.scenario_box)
+
+        self.layout_grid.addWidget(self.table, 0, 0)
+
+        self.consolmethod_box.currentIndexChanged.connect(self.method_check)
+
+    def method_check(self):
+        current_text = self.consolmethod_box.itemText(self.consolmethod_box.currentIndex())
+        if current_text == 'DISPOSEDDY':
+            self.table.setColumnHidden(9, False)
+        else:
+            self.table.setColumnHidden(9, True)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
